25e4b0dee8e8770d5afa91b12b7f3b4c070e179a
Yitzik Casapu <yitzikc@gmail.com>
2020-05-30 23:52:26 +0100

    Save recording data in a more usable, combined format

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 # Derived from an example here: https://www.twilio.com/blog/transcribe-voicemails-python-flask-twilio
 
 import os
+from typing import Mapping, Any
+from datetime import datetime
 
 from flask import Flask, request, abort
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.rest import Client
 from requests import get
 from urllib.parse import urlparse
-from json import dumps
+from json import dumps, load
 import boto3
 import pprint
 
@@ -63,11 +65,30 @@ def record_complete():
     capture_json(f"transcribe-twilio-{SLOT}-content", f"recordings/{sid}.json", request.form)
 
     if 'RecordingUrl' in request.form:
-        _save_twilio_recording_content(request.form)
+        mp3_path = _save_twilio_recording_content(request.form)
+    else:
+        mp3_path = None
 
-    return request.form['RecordingSid']
+    call_sid = request.form.get("CallSid")
+    if not call_sid:
+        abort(400, "Missing call Sid")
 
-# FIXME: Rename this to reflect the intent
+    call_record = _get_call_incoming_record(call_sid)
+    call_details = {
+        "Caller": _get_canonical_number(call_record) or None,
+        "RecordingStartTime": request.form["RecordingStartTime"],
+        "Mp3Path": mp3_path,
+        "RecordingSid": sid,
+        "CallSid": call_sid,
+    }
+
+    capture_json(
+        f"transcribe-twilio-{SLOT}-content",
+        _path_for_call_by_caller(call_details, request.form) + ".json",
+        call_details)
+    return sid
+
+
 @app.route("/twilio_transcription_complete", methods=["POST"])
 def twilio_transcription_complete():
     """Store the transcription received from Twilio. If non was given,
@@ -85,15 +106,36 @@ def twilio_transcription_complete():
     capture_json(f"transcribe-twilio-{SLOT}-content", f"transcriptions/twilio/{sid}.json", content)
     return str(sid)
 
-def _save_twilio_recording_content(args: dict):
+def _save_twilio_recording_content(args: dict) -> str:
     mp3_url = args['RecordingUrl'] + ".mp3"
     content = get(mp3_url).content
+    mp3_path = f"recordings/{args['RecordingSid']}.mp3"
     s3_obj = boto3.resource('s3').Object(
         f"transcribe-twilio-{SLOT}-content",
-        f"recordings/{args['RecordingSid']}.mp3"
+        mp3_path
     )
     s3_obj.put(Body=content)
-    return
+    return mp3_path
+
+
+def _get_canonical_number(form: Mapping[str, Any]) -> str:
+    return form.get("Caller", "").lstrip(" +")
+
+
+def _path_for_call_by_caller(call_details: Mapping[str, Any], form: Mapping[str, Any]) -> str:
+    caller_number = _get_canonical_number(call_details) or "UNKNOWN"
+    # Note: this is very brittle and might depend on being run in a locale set for English.
+    # At the very least we should control the locale that is used to run the function
+    call_time = datetime.strptime(form["RecordingStartTime"], "%a, %d %b %Y %H:%M:%S %z")
+    return f"by_caller/{caller_number}/{call_time.isoformat()}"
+
+
+def _get_call_incoming_record(call_sid: str) -> Mapping[str, Any]:
+    s3_obj = boto3.resource('s3').Object(
+        f"transcribe-twilio-{SLOT}-content", f"calls/{call_sid}.json")
+    js = load(s3_obj.get()['Body'])
+    return js
+
 
 def _fetch_twilio_transcription(sid) -> str:
     # Note: the code below was never successfully tested.
