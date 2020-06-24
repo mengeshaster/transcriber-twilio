abd45686ff8b488f1128892d8c4679a4735aab8d
Yitzik Casapu <yitzikc@gmail.com>
2020-06-24 23:46:35 +0100

    Define the app name and slot by arguments to sls

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
-# FIXME: Don't hard-code anything. Get the resource names from the environment
+import os
 
-APP_NAME="transcribe-twilio"
-SLOT = 'dev'
+APP_NAME = os.environ["APP_NAME"]
+SLOT = os.environ.get("SLOT", "dev")
+
 BUCKET = f"{APP_NAME}-{SLOT}-content"
 AWS_TRANSCRIBE_RESULTS_BUCKET = f"{APP_NAME}-{SLOT}-aws-transcriptions"
